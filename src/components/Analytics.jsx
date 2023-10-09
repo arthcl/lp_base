58bd015700e4e78c983276a7e0c3728b5290c235
arturo.c.lamas@gmail.com <arturo.c.lamas@gmail.com>
2023-10-09 12:48:38 -0300

    add Cards component

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Factory from '../assets/factory.jpg';
+import Laptop from '../assets/laptop.jpg';
 
 export const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-            <img className='w-[500px] mx-auto my-4' src={Factory} alt="/" />
+            <img className='w-[500px] mx-auto my-4' src={Laptop} alt="/" />
             <div className='flex flex-col justify-center'>
             <p className='text-[#00df9a] font-bold'>DATA ANALAYTICS DASHBOARD</p>
             <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage data analytics Centrally </h1>
